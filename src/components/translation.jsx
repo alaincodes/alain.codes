5cdc7ea958d8987c42d23609ee4818ee88a72d72
Alain <alaint77@gmail.com>
2019-08-03 14:13:30 +0200

    using react spring to add more LIIIFEEE

--- src/components/translation.jsx
+++ src/components/translation.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useSpring, animated } from "react-spring"
 
 class Translation extends React.Component {
   constructor(props) {
@@ -36,63 +37,78 @@ class Translation extends React.Component {
 }
 
 function French(props) {
+  const anim = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 900 },
+  })
   return (
     <div>
-      <h1 className="main-h1">
-        Hello, Je suis Alain,{" "}
-        <span className="main-highlight">Développeur Front-end Junior</span>. Je
-        code des applications{" "}
-        <span className="main-highlight">Web et Mobile</span> avec{" "}
-        <span className="main-highlight">ReactJS</span>.
-      </h1>
-      <h2 className="main-h2">
-        Je suis disponible pour completer votre équipe ou projet en freelance,
-        n'hésitez pas à me contacter sur
-        <span>
-          {" "}
-          <a
-            className="main-highlight"
-            href="https://twitter.com/alaincodes"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>{" "}
-        </span>{" "}
-        ou par Email:
-        <span className="main-highlight"> contact@alain.codes</span>
-      </h2>
+      <animated.div style={anim}>
+        <h1 className="main-h1">
+          Hello, Je suis Alain,{" "}
+          <span className="main-highlight">Développeur Front-end Junior</span>.
+          Je code des applications{" "}
+          <span className="main-highlight">Web et Mobile</span> avec{" "}
+          <span className="main-highlight">ReactJS</span>.
+        </h1>
+        <h2 className="main-h2">
+          Je suis disponible pour completer votre équipe ou projet en freelance,
+          n'hésitez pas à me contacter sur
+          <span>
+            {" "}
+            <a
+              className="main-highlight"
+              href="https://twitter.com/alaincodes"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>{" "}
+          </span>{" "}
+          ou par Email:
+          <span className="main-highlight"> contact@alain.codes</span>
+        </h2>
+      </animated.div>
     </div>
   )
 }
 
 function English(props) {
+  const anim = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 900 },
+  })
   return (
     <div>
-      <h1 className="main-h1">
-        Hello, I'm Alain{" "}
-        <span className="main-highlight">Junior Front-end Developer</span>. I
-        like to code for <span className="main-highlight">Web and Mobile </span>
-        devices using <span className="main-highlight">ReactJS</span>.
-      </h1>
-      <h2 className="main-h2">
-        I'm available to complete your team and cool freelance/remote
-        opportunities, hit me up on
-        <span>
-          {" "}
-          <a
-            className="main-highlight"
-            href="https://twitter.com/alaincodes"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>{" "}
-        </span>{" "}
-        or send me an Email:
-        <span className="main-highlight"> contact@alain.codes</span> for future
-        projects!
-      </h2>
+      <animated.div style={anim}>
+        <h1 className="main-h1">
+          Hello, I'm Alain{" "}
+          <span className="main-highlight">Junior Front-end Developer</span>. I
+          like to code for{" "}
+          <span className="main-highlight">Web and Mobile </span>
+          devices using <span className="main-highlight">ReactJS</span>.
+        </h1>
+        <h2 className="main-h2">
+          I'm available to complete your team and cool freelance/remote
+          opportunities, hit me up on
+          <span>
+            {" "}
+            <a
+              className="main-highlight"
+              href="https://twitter.com/alaincodes"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>{" "}
+          </span>{" "}
+          or send me an Email:
+          <span className="main-highlight"> contact@alain.codes</span> for
+          future projects!
+        </h2>
+      </animated.div>
     </div>
   )
 }
--- src/components/translationAbout.jsx
+++ src/components/translationAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useSpring, animated } from "react-spring"
 
 class TranslateAbout extends React.Component {
   constructor(props) {
@@ -36,43 +37,58 @@ class TranslateAbout extends React.Component {
 }
 
 function French(props) {
+  const anim = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 900 },
+  })
   return (
     <div>
-      <h1 className="about-h1">About</h1>
-      <p className="about-p">
-        Je développe des applications{" "}
-        <span className="about-highlight">Web et Mobile</span> avec{" "}
-        <span className="about-highlight">ReactJS</span>. Je travaille avec
-        beaucoup d'appétit, d'honnêté et d'engagement. J'attache beaucoup
-        d'importance aux <span className="about-highlight">design UI/UX</span>,
-        à l'<span className="about-highlight">Accessibilité</span> et aux{" "}
-        <span className="about-highlight">Performances</span>.
-      </p>
-      <p className="about-p">
-        Anciennement cuisinier à Paris, quand je ne code pas, vous me trouverez
-        certainement entrain de regarder ou de lire des livres/vidéos de cuisine
-        ou simplement derrière les fourneaux !
-      </p>
+      <animated.div style={anim}>
+        <h1 className="about-h1">About</h1>
+        <p className="about-p">
+          Je développe des applications{" "}
+          <span className="about-highlight">Web et Mobile</span> avec{" "}
+          <span className="about-highlight">ReactJS</span>. Je travaille avec
+          beaucoup d'appétit, d'honnêté et d'engagement. J'attache beaucoup
+          d'importance aux <span className="about-highlight">design UI/UX</span>
+          , à l'<span className="about-highlight">Accessibilité</span> et aux{" "}
+          <span className="about-highlight">Performances</span>.
+        </p>
+        <p className="about-p">
+          Anciennement cuisinier à Paris, quand je ne code pas, vous me
+          trouverez certainement entrain de regarder ou de lire des
+          livres/vidéos de cuisine ou simplement derrière les fourneaux !
+        </p>
+      </animated.div>
     </div>
   )
 }
 
 function English(props) {
+  const anim = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 900 },
+  })
   return (
     <div>
-      <h1 className="about-h1">About</h1>
-      <p className="about-p">
-        I build stuff for <span className="about-highlight">Web & Mobile</span>{" "}
-        devices using <span className="about-highlight">ReactJS</span>. I work
-        with a lot of appetite, honesty and commitment. I also like create nice{" "}
-        <span className="about-highlight">UI/UX Design</span> and follow
-        <span className="about-highlight"> Accessibility</span> guidelines.
-      </p>
-      <p className="about-p">
-        Outside of programming, you will probably see me reading, watching
-        cooking stuff or in front of the stove, I was working as a cook in Paris
-        years ago !!
-      </p>
+      <animated.div style={anim}>
+        <h1 className="about-h1">About</h1>
+        <p className="about-p">
+          I build stuff for{" "}
+          <span className="about-highlight">Web & Mobile</span> devices using{" "}
+          <span className="about-highlight">ReactJS</span>. I work with a lot of
+          appetite, honesty and commitment. I also like create nice{" "}
+          <span className="about-highlight">UI/UX Design</span> and follow
+          <span className="about-highlight"> Accessibility</span> guidelines.
+        </p>
+        <p className="about-p">
+          Outside of programming, you will probably see me reading, watching
+          cooking stuff or in front of the stove, I was working as a cook in
+          Paris years ago !!
+        </p>
+      </animated.div>
     </div>
   )
 }
